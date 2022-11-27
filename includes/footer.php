b3700d029a1bdd7eddd2598eab8554e97b49d4ee
Milos Naskovic <milosnaskovic17@gmail.com>
2022-11-27 21:04:41 +0100

    Created home page concept

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer d-flex justify-content-center align-items-center pt-3 mt-5">
+    <p>Turistička agencija kaktus &copy; <?php echo date('Y'); ?></p>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src=" assets/bootstrap/js/bootstrap.min.js">
+</script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php require_once('includes/header.php') ?>
+
+<div>
+    <!-- Slider -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./assets/img/slider/placeholder.png" class="d-block w-100" alt="slide-1">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/img/slider/placeholder.png" class="d-block w-100" alt="slide-2">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./assets/img/slider/placeholder.png" class="d-block w-100" alt="slide-3">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <!-- Content -->
+    <div class="container">
+        <!-- Header -->
+        <div class="mt-5 text-center">
+            <p>Putujte sa nama i budite uvek:</p>
+            <h1>Za kilometar ispred svih...</h1>
+        </div>
+
+        <!-- Destinations -->
+        <div class="mt-5">
+            <h2>Dostupne destinacije</h2>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 destination-row">
+                <div class="col destination-col">
+                    <div class="card h-100">
+                        <img src="./assets/img/slider/placeholder.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col destination-col">
+                    <div class="card h-100">
+                        <img src="./assets/img/slider/placeholder.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a short card.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col destination-col">
+                    <div class="card h-100">
+                        <img src="./assets/img/slider/placeholder.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col destination-col">
+                    <div class="card h-100">
+                        <img src="./assets/img/slider/placeholder.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Todo: Valuation  -->
+    </div>
+</div>
+
+<?php require_once('includes/footer.php') ?>
